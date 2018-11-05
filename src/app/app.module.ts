125d7c116a2eb8eaf04c35dbdbd98e371ea561c5
riku6460 <17585784+riku6460@users.noreply.github.com>
2018-11-06 01:46:58 +0900

    bootstrapついか

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarModule } from './core/navbar/navbar.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +15,8 @@ import { environment } from '../environments/environment';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NgbModule,
+    NavbarModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
