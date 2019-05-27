9f440300b6311097dde76d5c1359b1c6ced667b8
CookieRamen <ark@arkjp.net>
2019-05-27 22:04:09 +0900

    Created LiveComponent.

---
+++ src/app/live/live.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LiveRoutingModule } from './live-routing.module';
+import { LiveComponent } from './live.component';
+import {HttpClientModule} from '@angular/common/http';
+
+@NgModule({
+  declarations: [LiveComponent],
+  imports: [
+    CommonModule,
+    LiveRoutingModule,
+    HttpClientModule
+  ]
+})
+export class LiveModule { }
