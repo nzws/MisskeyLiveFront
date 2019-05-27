b900a5e649a9c8338718abbef9b27abbac941c12
CookieRamen <ark@arkjp.net>
2019-05-27 22:06:38 +0900

    Added routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: './index/index.module#IndexModule'
+  }, {
+    path: ':id',
+    loadChildren: './live/live.module#LiveModule'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
