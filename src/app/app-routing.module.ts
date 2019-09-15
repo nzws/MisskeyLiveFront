72537b32193c1f66b4074f7def513dffc03f4db6
CookieRamen <ark@arkjp.net>
2019-09-15 15:44:56 +0900

    Add AuthGuard to dashboard routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './core/guard/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./index/index.module').then(m => m.IndexModule)
+  }, {
+    path: 'dashboard',
+    loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule),
+    canActivate: [AuthGuard]
   }, {
     path: ':id',
     loadChildren: () => import('./live/live.module').then(m => m.LiveModule)
