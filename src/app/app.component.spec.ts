365faae5d524a48aa9f9185073ede95567603ae7
CookieRamen <ark@arkjp.net>
2019-09-23 18:47:40 +0900

    testのこと考えてなかった

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,6 +3,7 @@ import {RouterTestingModule} from '@angular/router/testing';
 import {AppComponent} from './app.component';
 import {CookieService} from 'ngx-cookie-service';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {NgbCollapseModule} from '@ng-bootstrap/ng-bootstrap';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -10,6 +11,7 @@ describe('AppComponent', () => {
       imports: [
         RouterTestingModule,
         HttpClientTestingModule,
+        NgbCollapseModule,
       ],
       declarations: [AppComponent],
       providers: [CookieService]
