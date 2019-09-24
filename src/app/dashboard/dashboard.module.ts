9bbf23ae6d43e1442354de544c263c99038b843c
CookieRamen <ark@arkjp.net>
2019-09-24 17:18:26 +0900

    Dashboard実装した

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,10 +3,19 @@ import {CommonModule} from '@angular/common';
 
 import {DashboardRoutingModule} from './dashboard-routing.module';
 import {DashboardComponent} from './dashboard.component';
+import {HttpClientModule} from '@angular/common/http';
+import {ClipboardModule} from 'ngx-clipboard';
+import {SweetAlert2Module} from '@sweetalert2/ngx-sweetalert2';
 
 @NgModule({
   declarations: [DashboardComponent],
-  imports: [CommonModule, DashboardRoutingModule]
+  imports: [
+    CommonModule,
+    DashboardRoutingModule,
+    HttpClientModule,
+    ClipboardModule,
+    SweetAlert2Module.forRoot(),
+  ]
 })
 export class DashboardModule {
 }
